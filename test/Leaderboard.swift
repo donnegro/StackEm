49262992843e8b6b4a20efe45f2ccba40160d420
Daniel Dao <daniel.vu.dao@gmail.com>
2015-11-09 20:36:06 -0600

    scores complete

--- test/Leaderboard.swift
+++ test/Leaderboard.swift
@@ -1,14 +1,59 @@
-//
-//  Leaderboard.swift
-//  test
-//
-//  Created by Don Negro on 10/28/15.
-//  Copyright © 2015 Don Negro. All rights reserved.
-//
-
-import Foundation
 import UIKit
+import ParseUI
+import Parse
 
-class Leaderboard : UITableViewController {
+class Leaderboard: PFQueryTableViewController {
+    var frendToAddTextField:UITextField? = nil
+    
+    override init(style: UITableViewStyle, className: String!){
+        super.init(style: style, className: className)
+    }
+    
+    required init!(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        self.parseClassName = "Game"
+        self.textKey = "Score"
+        self.pullToRefreshEnabled = true
+        self.paginationEnabled = false
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    }
+    
+    
+    
+    override func queryForTable() -> PFQuery {
+        let query:PFQuery = PFQuery(className: "Game")
+        query.orderByDescending("Score")
+        
+        return query
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
+        var cell:PFTableViewCell = tableView.dequeueReusableCellWithIdentifier("scoreCell") as! PFTableViewCell
+        
+        cell.textLabel?.text = String(object?["Score"] as! Int) + " by " + ((object?["userName"])! as! String) as? String
+        
+    
+        return cell
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        tableView.reloadData()
+    }
     
 }
